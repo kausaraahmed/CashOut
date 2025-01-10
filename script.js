f21357b60e36f900adeef6555f4376f70835e715
Kausar Ahmed <kausarahmed2nd@gmail.com>
2025-01-10 17:19:46 +0600

    updated logics and mobilr responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,82 @@
+var rateDict = {
+    bPrio: 0.0149,
+    bM: 0.0185,
+    bATM: 0.0185,
+    nM: 0.0125,
+    rM: 0.018,
+    rATM: 0.009,
+};
 
+selectedRate = 0.0;
+const b = document.getElementById('b')
+const n = document.getElementById('n')
+const r = document.getElementById('r')
+const b_option = document.getElementById('b-option')
+const n_option = document.getElementById('n-option')
+const r_option = document.getElementById('r-option')
+
+function setDefault() {
+    selectedRate = 0.0;
+    document.getElementById('show_charge').innerText = '___';
+
+    const buttons = document.querySelectorAll('.charge-option');
+    buttons.forEach((button) => {
+        button.style.opacity = 0.4;
+    });
+}
+
+function isSelected(id) {
+    setDefault();
+    selectedRate = rateDict[id];
+    document.getElementById('show_charge').innerText = selectedRate * 1000;
+    document.getElementById(id).style.opacity = 1;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    b.style.opacity = '1'
+    n.style.opacity = '0.2'
+    r.style.opacity = '0.2'
+    setDefault();
+});
+
+document.getElementById('b').addEventListener('click', () => {
+    b.style.opacity = '1'
+    n.style.opacity = '0.2'
+    r.style.opacity = '0.2'
+    b_option.style.display = 'flex'
+    n_option.style.display = 'none'
+    r_option.style.display = 'none'
+    setDefault();
+});
+
+document.getElementById('n').addEventListener('click', () => {
+    n.style.opacity = '1'
+    b.style.opacity = '0.2'
+    r.style.opacity = '0.2'
+    b_option.style.display = 'none'
+    n_option.style.display = 'flex'
+    r_option.style.display = 'none'
+    setDefault();
+});
+
+document.getElementById('r').addEventListener('click', () => {
+    r.style.opacity = '1'
+    n.style.opacity = '0.2'
+    b.style.opacity = '0.2'
+    b_option.style.display = 'none'
+    n_option.style.display = 'none'
+    r_option.style.display = 'flex'
+    setDefault();
+});
 
 document.getElementById('calculate').addEventListener('click', () => {
     let balance = document.getElementById('balance').value;
     let remainingValue = document.getElementById('remaining').value;
-
-    let rate = document.querySelector('input[name="charge"]:checked').value;
-    console.log(rate);
-    rate = parseFloat(rate);
+    let rate = selectedRate;
+    if (rate == 0.0) {
+        alert('Please Select Rate');
+        return;
+    }
 
     if (!balance) {
         alert('Please Enter Current Balance');
@@ -46,54 +116,4 @@ document.getElementById('calculate').addEventListener('click', () => {
     document.getElementById('cashout').innerText = cashout.toFixed(2);
     document.getElementById('charge').innerText = charge.toFixed(2);
     document.getElementById('reBal').innerText = remain.toFixed(2);
-});
-
-const b = document.getElementById('b')
-const n = document.getElementById('n')
-const r = document.getElementById('r')
-const b_option = document.getElementById('b-option')
-const n_option = document.getElementById('n-option')
-const r_option = document.getElementById('r-option')
-
-document.addEventListener("DOMContentLoaded", () => {
-    b.style.borderColor = 'red'
-    n.style.borderColor = 'black'
-    r.style.borderColor = 'black'
-    b.style.opacity = '1'
-    n.style.opacity = '0.2'
-    r.style.opacity = '0.2'
-});
-
-document.getElementById('b').addEventListener('click', () => {
-    b.style.borderColor = 'red'
-    n.style.borderColor = 'black'
-    r.style.borderColor = 'black'
-    b.style.opacity = '1'
-    n.style.opacity = '0.2'
-    r.style.opacity = '0.2'
-    b_option.style.display = 'flex'
-    n_option.style.display = 'none'
-    r_option.style.display = 'none'
-});
-document.getElementById('n').addEventListener('click', () => {
-    n.style.borderColor = 'red'
-    b.style.borderColor = 'black'
-    r.style.borderColor = 'black'
-    n.style.opacity = '1'
-    b.style.opacity = '0.2'
-    r.style.opacity = '0.2'
-    b_option.style.display = 'none'
-    n_option.style.display = 'flex'
-    r_option.style.display = 'none'
-});
-document.getElementById('r').addEventListener('click', () => {
-    r.style.borderColor = 'red'
-    b.style.borderColor = 'black'
-    n.style.borderColor = 'black'
-    r.style.opacity = '1'
-    n.style.opacity = '0.3'
-    b.style.opacity = '0.3'
-    b_option.style.display = 'none'
-    n_option.style.display = 'none'
-    r_option.style.display = 'flex'
 });
